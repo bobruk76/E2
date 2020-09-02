563bcddb4b4943cd250fefe81526bca7d84594bf
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-09-02 17:37:34 +0300

    add sendgrid

--- mailapp/sendgrid_send_email.py
+++ mailapp/sendgrid_send_email.py
@@ -27,3 +27,18 @@ def send_email(from_email, to_email, template_id, template_data):
 if __name__ == '__main__':
     send_email(from_email='from_template@gmail.com', to_email='your_address@gmail.com', 
            template_id=SENDGRID_TEMPLATE_ID, template_data=data)
+
+
+# message = Mail(
+#     from_email='from_email@example.com',
+#     to_emails='to@example.com',
+#     subject='Sending with Twilio SendGrid is Fun',
+#     html_content='<strong>and easy to do anywhere, even with Python</strong>')
+# try:
+#     sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+#     response = sg.send(message)
+#     print(response.status_code)
+#     print(response.body)
+#     print(response.headers)
+# except Exception as e:
+#     print(e.message)
